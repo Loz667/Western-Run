ba90bfab15ecbe3daf869b0d99cbe3c57be77a0a
Loren Penny <loren_667@hotmail.com>
2023-03-30 12:54:31 +0100

    Updated spawn script to include obstacles

--- Western Run/Assets/Scripts/Spawner.cs
+++ Western Run/Assets/Scripts/Spawner.cs
@@ -41,7 +41,68 @@ namespace WesternRun
             Quaternion newTileRotation = tile.gameObject.transform.rotation * Quaternion.LookRotation(currentTileDirection, Vector3.up);
             prevTile = GameObject.Instantiate(tile.gameObject, currentTileLocation, newTileRotation);
             currentTiles.Add(prevTile);
-            currentTileLocation += Vector3.Scale(prevTile.GetComponent<Renderer>().bounds.size, currentTileDirection);
+
+            if (spawnObstacle) SpawnObstacle();
+
+            if (tile.type == TileType.Straight)
+                currentTileLocation += Vector3.Scale(prevTile.GetComponent<Renderer>().bounds.size, currentTileDirection);
+        }
+
+        public void AddNewDirection(Vector3 direction)
+        {
+            currentTileDirection = direction;
+            DeletePrevious();
+
+            Vector3 tilePlacementScale;
+            if (prevTile.GetComponent<Tile>().type == TileType.Sideways)
+            {
+                tilePlacementScale = Vector3.Scale(prevTile.GetComponent<Renderer>().bounds.size / 2 + 
+                                    (Vector3.one * startTile.GetComponent<BoxCollider>().size.z / 2), currentTileDirection);
+            }
+            else
+            {
+                tilePlacementScale = Vector3.Scale((prevTile.GetComponent<Renderer>().bounds.size - (Vector3.one * 2)) +
+                                    (Vector3.one * startTile.GetComponent<BoxCollider>().size.z / 2), currentTileDirection);
+            }
+
+            currentTileLocation += tilePlacementScale;
+
+            int currentPathLength = Random.Range(minStraightTiles, maxStraightTiles);
+            for (int i = 0; i < currentPathLength; i++)
+            {
+                SpawnTile(startTile.GetComponent<Tile>(), (i == 0) ? false : true);
+            }
+
+            SpawnTile(SelectRandomGameObjectFromList(turnTiles).GetComponent<Tile>());
+        }
+
+        void DeletePrevious()
+        {
+            while (currentTiles.Count != 1)
+            {
+                GameObject tile = currentTiles[0];
+                currentTiles.RemoveAt(0);
+                Destroy(tile);
+            }
+
+            while (currentObstacles.Count != 0)
+            {
+                GameObject obstacle = currentObstacles[0];
+                currentObstacles.RemoveAt(0);
+                Destroy(obstacle);
+            }
+        }
+
+        private void SpawnObstacle()
+        {
+            if (Random.value > 0.2f) return;
+
+            GameObject obstaclePrefab = SelectRandomGameObjectFromList(obstacles);
+            Quaternion newObstacleRotation = obstaclePrefab.gameObject.transform.rotation * 
+                                                Quaternion.LookRotation(currentTileDirection, Vector3.up);
+
+            GameObject obstacle = Instantiate(obstaclePrefab, currentTileLocation, newObstacleRotation);
+            currentObstacles.Add(obstacle);
         }
 
         private GameObject SelectRandomGameObjectFromList(List<GameObject> list)
